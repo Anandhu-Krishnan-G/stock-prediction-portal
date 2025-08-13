f7cdcc9bd6fcdcd107a8e26a29bc0959096c6580
Anandhu-Krishnan-G <anandhukrishnang142@gmail.com>
2025-08-13 18:01:55 +0530

    intergrating machine learning model with django and react application

--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -151,3 +151,7 @@ SIMPLE_JWT ={
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
+
+# Media File  configurations
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
